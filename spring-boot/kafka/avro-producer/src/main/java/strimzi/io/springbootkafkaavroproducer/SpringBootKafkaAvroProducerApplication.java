2ddc8f4554e0f11e8c3818a53efd805ea63cd5fe
cbuchberger <c.buchberger@witcom.de>
2021-01-05 09:58:10 +0100

    Added Avro-Producer

---
+++ spring-boot/kafka/avro-producer/src/main/java/strimzi/io/springbootkafkaavroproducer/SpringBootKafkaAvroProducerApplication.java
@@ -0,0 +1,13 @@
+package strimzi.io.springbootkafkaavroproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaAvroProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaAvroProducerApplication.class, args);
+	}
+
+}
---
+++ spring-boot/kafka/avro-producer/src/main/java/strimzi/io/springbootkafkaavroproducer/service/UserProducer.java
@@ -0,0 +1,47 @@
+package strimzi.io.springbootkafkaavroproducer.service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import strimzi.io.springbootkafkaavroproducer.User;
+
+@Service
+public class UserProducer {
+	private static final org.apache.commons.logging.Log log = org.apache.commons.logging.LogFactory.getLog(UserProducer.class);
+
+	  @Value("${topic.name}") 
+	  private String TOPIC;
+
+	  private final KafkaTemplate<String, User> kafkaTemplate;
+
+	  @Autowired
+	  public UserProducer(KafkaTemplate<String, User> kafkaTemplate) {   
+	    this.kafkaTemplate = kafkaTemplate;
+	  }
+
+	  void sendMessage(User user) {
+	    this.kafkaTemplate.send(this.TOPIC, user.getName(), user);  
+	    log.info(String.format("Produced user -> %s", user));
+	  }
+	  
+	  @Scheduled(fixedRate = 5000)
+	  public void produce() {
+		  
+		  List<String> givenList = Arrays.asList("hans", "peter", "paul","mary","joe");
+		  Random rand = new Random();
+		  String randomElement = givenList.get(rand.nextInt(givenList.size()));
+		  User user = new User();
+		  user.setName(randomElement);
+		  user.setAge(rand.ints(10, 100).findFirst().getAsInt());
+		  this.sendMessage(user);
+		
+	  }
+
+}
